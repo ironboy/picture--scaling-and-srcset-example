a3a8953c7225fc1c6dc1452a25b38514aa4e21fa
Ironboy <ironboy@users.noreply.github.com>
2024-11-14 08:03:06 +0100

    added timing info

--- image-scaler.js
+++ image-scaler.js
@@ -31,17 +31,21 @@ fs.mkdirSync(destFolder);
 // save to different sizes in the webp format, in our destination folder
 // (using default webp quality options,
 //  see https://sharp.pixelplumbing.com/api-output#webp)
+let startTime = Date.now();
 for (let file of srcFilePaths) {
   let subFolderPath = file.split(path.sep).slice(0, -1).join(path.sep);
   fs.mkdirSync(path.join(destFolder, subFolderPath), { recursive: true });
   console.log('Original file: ' + file, 'size: ', getFileSize(path.join(srcFolder, file)));
+  let startTimeOne = Date.now();
   for (let width of scaleToWidths) {
     const destPath = file.slice(0, file.lastIndexOf('.')) + '-w' + width + '.webp';
     await sharp(path.join(srcFolder, file)).resize({ width }).webp().toFile(path.join(destFolder, destPath));
     console.log('  ', destPath, getFileSize((path.join(destFolder, destPath))));
   }
+  console.log('Conversion time:', ((Date.now() - startTimeOne) / 1000).toFixed(1), 'seconds');
   console.log('');
 }
+console.log('Total conversion time:', ((Date.now() - startTime) / 1000).toFixed(1), 'seconds\n');
 
 function getFileSize(filePath) {
   return Math.round((fs.statSync(filePath).size / 1024)) + ' kb';
